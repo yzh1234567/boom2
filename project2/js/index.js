842d22fb2716494a33f7e4426275454429d5c0e9
yzh1234567 <45424767+yzh1234567@users.noreply.github.com>
2018-12-07 09:45:19 +0800

    Initial commit

---
+++ project2/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#my_header").load("././header.html");
+    //在页面装载时，在ID为#labels的DOM元素里插入test.html的内容。
+});
+$(document).ready(function(){
+    $("#my_footer").load("././footer.html");
+    //在页面装载时，在ID为#labels的DOM元素里插入test.html的内容。
+});
